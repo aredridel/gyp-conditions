2405cda86192a82ecbb0ada6accdfdad7bba62b2
Aria Stewart <aredridel@nbtsc.org>
2013-06-16 02:04:33 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var ometajs_ = require("ometajs");
+
+var AbstractGrammar = ometajs_.grammars.AbstractGrammar;
+
+var BSJSParser = ometajs_.grammars.BSJSParser;
+
+var BSJSIdentity = ometajs_.grammars.BSJSIdentity;
+
+var BSJSTranslator = ometajs_.grammars.BSJSTranslator;
