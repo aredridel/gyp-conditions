a06bf62feaf4dcc3cc7320be646af590632f9e41
Aria Stewart <aredridel@nbtsc.org>
2013-06-30 01:01:04 -0400

    Add other numeric literals

--- index.js
+++ index.js
@@ -51,27 +51,162 @@ Condition.prototype["identifier"] = function $identifier() {
     }, true) && (v = this._getIntermediate(), true) && this._exec(this._options.variables[v]);
 };
 
-Condition.prototype["number"] = function $number() {
-    return this._atomic(function() {
-        return this._many(function() {
-            return this._atomic(function() {
-                return this._rule("digit", false, [], null, this["digit"]);
-            });
-        }) && this._match(".") && this._many(function() {
-            return this._atomic(function() {
-                return this._rule("digit", false, [], null, this["digit"]);
-            });
-        });
+Condition.prototype["floatnumber"] = function $floatnumber() {
+    var f;
+    return (this._atomic(function() {
+        return this._rule("exponentfloat", false, [], null, this["exponentfloat"]);
     }) || this._atomic(function() {
-        return this._many(function() {
-            return this._atomic(function() {
-                return this._rule("digit", false, [], null, this["digit"]);
-            });
-        });
+        return this._rule("pointfloat", false, [], null, this["pointfloat"]);
+    })) && (f = this._getIntermediate(), true) && this._exec(parseFloat(f));
+};
+
+Condition.prototype["pointfloat"] = function $pointfloat() {
+    return this._atomic(function() {
+        var x, y;
+        return this._optional(function() {
+            return this._rule("intpart", false, [], null, this["intpart"]);
+        }) && (x = this._getIntermediate(), true) && this._rule("fraction", false, [], null, this["fraction"]) && (y = this._getIntermediate(), 
+        true) && this._exec(x + "." + y);
+    }) || this._atomic(function() {
+        var x;
+        return this._rule("intpart", false, [], null, this["intpart"]) && (x = this._getIntermediate(), 
+        true) && this._match(".") && this._exec(x + ".0");
     });
 };
 
-Condition.prototype["string"] = function $string() {
+Condition.prototype["exponentfloat"] = function $exponentfloat() {
+    var f, e;
+    return (this._atomic(function() {
+        return this._rule("intpart", false, [], null, this["intpart"]);
+    }) || this._atomic(function() {
+        return this._rule("pointfloat", false, [], null, this["pointfloat"]);
+    })) && (f = this._getIntermediate(), true) && this._rule("exponent", false, [], null, this["exponent"]) && (e = this._getIntermediate(), 
+    true) && this._exec(f + "e" + e);
+};
+
+Condition.prototype["intpart"] = function $intpart() {
+    var v;
+    return this._many(function() {
+        return this._atomic(function() {
+            return this._rule("digit", false, [], null, this["digit"]);
+        });
+    }) && (v = this._getIntermediate(), true) && this._exec(v.join(""));
+};
+
+Condition.prototype["fraction"] = function $fraction() {
+    var y;
+    return this._match(".") && this._many(function() {
+        return this._atomic(function() {
+            return this._rule("digit", false, [], null, this["digit"]);
+        });
+    }) && (y = this._getIntermediate(), true) && this._exec(y.join(""));
+};
+
+Condition.prototype["exponent"] = function $exponent() {
+    var e;
+    return (this._match("e") || this._match("E")) && this._optional(function() {
+        return this._match("+") || this._match("-");
+    }) && this._many(function() {
+        return this._atomic(function() {
+            return this._rule("digit", false, [], null, this["digit"]);
+        });
+    }) && (e = this._getIntermediate(), true);
+};
+
+Condition.prototype[""] = function $() {
+    return this._exec(e.join(""));
+};
+
+Condition.prototype["longinteger"] = function $longinteger() {
+    var i;
+    return this._rule("integer", false, [], null, this["integer"]) && (i = this._getIntermediate(), 
+    true) && (this._match("L") || this._match("l")) && this._exec(i);
+};
+
+Condition.prototype["integer"] = function $integer() {
+    return this._atomic(function() {
+        return this._rule("bininteger", false, [], null, this["bininteger"]);
+    }) || this._atomic(function() {
+        return this._rule("octinteger", false, [], null, this["octinteger"]);
+    }) || this._atomic(function() {
+        return this._rule("hexinteger", false, [], null, this["hexinteger"]);
+    }) || this._atomic(function() {
+        return this._rule("decimalinteger", false, [], null, this["decimalinteger"]);
+    });
+};
+
+Condition.prototype["decimalinteger"] = function $decimalinteger() {
+    return this._atomic(function() {
+        var v;
+        return this._atomic(function() {
+            return this._rule("nonzerodigit", false, [], null, this["nonzerodigit"]) && this._any(function() {
+                return this._atomic(function() {
+                    return this._rule("digit", false, [], null, this["digit"]);
+                });
+            });
+        }) && (v = this._getIntermediate(), true) && this._exec(parseInt(v, 10));
+    }) || this._atomic(function() {
+        return this._match("0") && this._exec(0);
+    });
+};
+
+Condition.prototype["octinteger"] = function $octinteger() {
+    return this._atomic(function() {
+        var v;
+        return this._seq(/^(0o)/i) && this._many(function() {
+            return this._atomic(function() {
+                return this._rule("octdigit", false, [], null, this["octdigit"]);
+            });
+        }) && (v = this._getIntermediate(), true) && this._exec(parseInt(v.join(""), 8));
+    }) || this._atomic(function() {
+        var v;
+        return this._atomic(function() {
+            return this._match("0") && this._many(function() {
+                return this._atomic(function() {
+                    return this._rule("octdigit", false, [], null, this["octdigit"]);
+                });
+            });
+        }) && (v = this._getIntermediate(), true) && this._exec(parseInt(v, 8));
+    });
+};
+
+Condition.prototype["hexinteger"] = function $hexinteger() {
+    var v;
+    return this._seq(/^(0x)/i) && this._many(function() {
+        return this._atomic(function() {
+            return this._rule("hexdigit", false, [], null, this["hexdigit"]);
+        });
+    }) && (v = this._getIntermediate(), true) && this._exec(parseInt(v.join(""), 16));
+};
+
+Condition.prototype["bininteger"] = function $bininteger() {
+    var v;
+    return this._seq(/^(0b)/i) && this._many(function() {
+        return this._atomic(function() {
+            return this._rule("bindigit", false, [], null, this["bindigit"]);
+        });
+    }) && (v = this._getIntermediate(), true) && this._exec(parseInt(v.join(""), 2));
+};
+
+Condition.prototype["nonzerodigit"] = function $nonzerodigit() {
+    return this._seq(/^([1-9])/);
+};
+
+Condition.prototype["octdigit"] = function $octdigit() {
+    return this._seq(/^([0-7])/);
+};
+
+Condition.prototype["bindigit"] = function $bindigit() {
+    return this._seq(/^([01])/);
+};
+
+Condition.prototype["hexdigit"] = function $hexdigit() {
+    return this._atomic(function() {
+        return this._rule("digit", false, [], null, this["digit"]);
+    }) || this._seq(/^([a-f])/i);
+};
+
+Condition.prototype["stringliteral"] = function $stringliteral() {
     var q, v;
     return (this._match("'") || this._match('"')) && (q = this._getIntermediate(), true) && this._atomic(function() {
         return !this._atomic(function() {
@@ -90,9 +225,13 @@ Condition.prototype["atom"] = function $atom() {
 
 Condition.prototype["literal"] = function $literal() {
     return this._atomic(function() {
-        return this._rule("string", false, [], null, this["string"]);
+        return this._rule("stringliteral", false, [], null, this["stringliteral"]);
     }) || this._atomic(function() {
-        return this._rule("number", false, [], null, this["number"]);
+        return this._rule("floatnumber", false, [], null, this["floatnumber"]);
+    }) || this._atomic(function() {
+        return this._rule("longinteger", false, [], null, this["longinteger"]);
+    }) || this._atomic(function() {
+        return this._rule("integer", false, [], null, this["integer"]);
     });
 };
 
@@ -156,3 +295,5 @@ module.exports = function(str, variables) {
     });
     return Evaluator.match(tree, "interp");
 };
+
+module.exports.parser = Condition;
--- test/basics.js
+++ test/basics.js
@@ -1,5 +1,8 @@
 var cond = require('../index');
 var test = require('tap').test;
+var parseLiteral = function(s) {
+    return cond.parser.matchAll(s, 'literal');
+};
 
 test("Basic conditions", function (t) {
     t.equal(cond('n>0', {n: 1}), true);
@@ -7,3 +10,14 @@ test("Basic conditions", function (t) {
     t.equal(cond('n<=1', {n: 1}), true);
     t.end();
 });
+
+test("Literals", function(t) {
+    t.equal(parseLiteral('0'), 0);
+    t.equal(parseLiteral('010'), 8);
+    t.equal(parseLiteral('0o10'), 8);
+    t.equal(parseLiteral('0x10'), 16);
+    t.equal(parseLiteral('2.0'), 2.0);
+    t.equal(parseLiteral('2.5'), 2.5);
+    t.end();
+});
+
