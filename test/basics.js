f7d20c7af346c289f9b0c9906de181c03e212949
Aria Stewart <aredridel@nbtsc.org>
2013-07-05 14:09:20 -0400

    Separate literals test

--- test/basics.js
+++ test/basics.js
@@ -15,22 +15,6 @@ test("Basic conditions", function (t) {
     t.end();
 });
 
-test("Literals", function(t) {
-    t.equal(parseLiteral('0'), 0, "Zero");
-    t.equal(parseLiteral('010'), 8, "Octal 10 with 0 prefix");
-    t.equal(parseLiteral('0o10'), 8, "Octal 10 with 0o prefix");
-    t.equal(parseLiteral('0x10'), 16, "Hex 10");
-    t.equal(parseLiteral('2.0'), 2.0, "Float 2.0");
-    t.equal(parseLiteral('2.5'), 2.5, "Float 2.5");
-    t.equal(parseLiteral('2.5e5'), 250000, "2.5e5");
-    t.equal(parseLiteral('2.5e-1'), 0.25, "2.5e-1");
-    t.equal(parseLiteral('"test"'), "test", "double quoted string");
-    t.equal(parseLiteral("'test'"), "test", "single quoted string");
-    t.equal(parseLiteral('"""test"""'), "test", "double quoted longstring");
-    t.equal(parseLiteral("'''test'''"), "test", "single quoted longstring");
-    t.end();
-});
-
 test("Escape Codes", function(t) {
     t.equal(parseLiteral('"\\n"'), "\n", "newline escape");
     t.equal(parseLiteral('"\\\\"'), "\\", "escaped backslash");
